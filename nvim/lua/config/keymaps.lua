fb9f4af8c7dfcda23d9bc8037942ef9076775fae
inwonakng <inwon.kang04@gmail.com>
2023-10-23 18:36:50 -0400

    better lsp config and keymaps for changging window

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,14 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>p", function()
+local map = vim.keymap.set
+
+map("n", "<leader>p", function()
   require("nabla").popup()
 end, { desc = "render latex equation" })
+
+-- Resize window using <ctrl> arrow keys
+map("n", "<C-S-k>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map("n", "<C-S-j>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map("n", "<C-S-l>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<C-S-h>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
