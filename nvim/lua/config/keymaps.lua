30f6de1d2b03c41974fe5ace9b093e0d17c14ac4
inwonakng <inwon.kang04@gmail.com>
2023-10-31 15:57:21 -0400

    nvim plugins cleaned

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -11,5 +11,9 @@ end, { desc = "render latex equation" })
 -- Resize window using <ctrl> arrow keys
 map("n", "<C-S-k>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
 map("n", "<C-S-j>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-map("n", "<C-S-l>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-map("n", "<C-S-h>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+map("n", "<C-S-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<C-S-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+
+
+-- Open diagnostic in floating window
+map("n", "<leader>i", ":lua vim.diagnostic.open_float(nil, {focus=false, scope='cursor'})<CR>", { desc = 'Toggle Diagnostics' })
--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -8,5 +8,10 @@ return {
       completion = cmp.config.window.bordered(),
       documentation = cmp.config.window.bordered(),
     }
+    table.insert(opts.sources, 1, {
+      name = "copilot",
+      group_index = 1,
+      priority = 100,
+    })
   end,
 }
