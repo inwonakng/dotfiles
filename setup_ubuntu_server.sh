ec98be054bf7a1339a9303f238c6171391cd0641
inwonakng <inwon.kang04@gmail.com>
2023-11-07 16:24:59 -0500

    adding bash config..

--- setup_ubuntu_server.sh
+++ setup_ubuntu_server.sh
@@ -1,4 +1,5 @@
 ln -sf ~/dotfiles/ubuntu-server/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/ubuntu-server/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/common/.vimrc  ~/.vimrc
 ln -sf ~/dotfiles/common/.ipython  ~/.ipython
 ln -sf ~/dotfiles/common/lazy-nvim-lite  ~/.config/nvim
