1e806ff113876688d7381131fba42fb05bafa482
inwonakng <inwon.kang04@gmail.com>
2023-11-21 14:36:05 -0500

    fixed symlink for ubuntu

--- setup_ubuntu_desktop.sh
+++ setup_ubuntu_desktop.sh
@@ -2,5 +2,5 @@ ln -sf ~/dotfiles/ubuntu-desktop/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/ubuntu-desktop/.latexmkrc ~/.latexmkrc
 ln -sf ~/dotfiles/common/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/common/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/common/.ipython ~/.ipython
-ln -sf ~/dotfiles/common/nvim ~/.config/nvim
+ln -snf ~/dotfiles/common/.ipython ~/.ipython
+ln -snf ~/dotfiles/common/nvim ~/.config/nvim
--- setup_ubuntu_server.sh
+++ setup_ubuntu_server.sh
@@ -3,5 +3,5 @@ bash install/conda.sh
 # ln -sf ~/dotfiles/ubuntu-server/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/ubuntu-server/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/common/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/common/.ipython ~/.ipython
-ln -sf ~/dotfiles/common/nvim-lite ~/.config/nvim
+ln -snf ~/dotfiles/common/.ipython ~/.ipython
+ln -snf ~/dotfiles/common/nvim-lite ~/.config/nvim
