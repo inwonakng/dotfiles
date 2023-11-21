c122f2e9d299a13b4b98e38029758ad7fae44cf1
inwonakng <inwon.kang04@gmail.com>
2023-11-21 01:35:29 -0500

    ubuntu server doesn't need zsh

--- setup_ubuntu_server.sh
+++ setup_ubuntu_server.sh
@@ -1,6 +1,6 @@
 bash install/conda.sh
 
-ln -sf ~/dotfiles/ubuntu-server/.zshrc ~/.zshrc
+# ln -sf ~/dotfiles/ubuntu-server/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/ubuntu-server/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/common/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/common/.ipython ~/.ipython
