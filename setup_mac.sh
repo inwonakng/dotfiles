7297186ed1dabeff8619b04d9264315cab58e75b
inwonakng <inwon.kang04@gmail.com>
2024-01-14 03:29:26 -0500

    moving stuff out of common..

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,14 +1,14 @@
-bash install/conda.sh
+bash scripts/conda.sh
 
 # files
 ln -sf ~/dotfiles/osx/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/osx/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/osx/.latexmkrc ~/.latexmkrc
 ln -sf ~/dotfiles/osx/.hammerspoon/init.lua ~/.hammerspoon/init.lua
-ln -sf ~/dotfiles/common/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/common/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 # folders
-ln -snf ~/dotfiles/common/.ipython ~/.ipython
-ln -snf ~/dotfiles/common/nvim ~/.config/nvim
-ln -snf ~/dotfiles/common/wezterm ~/.config/wezterm
+ln -snf ~/dotfiles/.ipython ~/.ipython
+ln -snf ~/dotfiles/nvim ~/.config/nvim
+ln -snf ~/dotfiles/wezterm ~/.config/wezterm
--- setup_ubuntu_server.sh
+++ setup_ubuntu_server.sh
@@ -1,8 +1,11 @@
-bash install/conda.sh
-bash install/pynvim.sh
+bash scripts/conda.sh
+bash scripts/pynvim.sh
 
-# ln -sf ~/dotfiles/ubuntu-server/.zshrc ~/.zshrc
+# files
 ln -sf ~/dotfiles/ubuntu-server/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/common/.vimrc ~/.vimrc
-ln -snf ~/dotfiles/common/.ipython ~/.ipython
-ln -snf ~/dotfiles/common/nvim-lite ~/.config/nvim
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+# folders
+ln -snf ~/dotfiles/.ipython ~/.ipython
+ln -snf ~/dotfiles/nvim-lite ~/.config/nvim
