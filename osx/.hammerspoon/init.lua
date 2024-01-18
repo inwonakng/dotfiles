9ae797f5499481c14d4986891275255526c6d0f3
inwonakng <inwon.kang04@gmail.com>
2024-01-18 04:14:13 -0500

    hammerspoon updated

--- osx/.hammerspoon/init.lua
+++ osx/.hammerspoon/init.lua
@@ -1,5 +1,20 @@
 -- https://www.hammerspoon.org/go/
 
 hs.hotkey.bind({"alt" }, "space", function()
-  hs.application.launchOrFocus("Wezterm")
+  local term = hs.application.get("Wezterm")
+  if term == nil then
+    hs.application.launchOrFocus("Wezterm")
+    return
+  else
+    if term:isFrontmost() then
+      term:hide()
+    else
+      term:setFrontmost()
+    end
+  end
 end)
+
+
+local movewindows = hs.hotkey.modal.new()
+
+hyper:bind({}, 'm', nil, function() movewindows:enter() end)
