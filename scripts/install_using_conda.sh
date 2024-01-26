38199e153abbe42d80a5166b6c93ea32f9860686
inwonakng <inwon.kang04@gmail.com>
2024-01-26 11:41:10 -0500

    standalone conda install script

---
+++ scripts/install_using_conda.sh
@@ -0,0 +1,40 @@
+# /usr/bin/bash
+
+program=""
+version=""
+
+print_usage() {
+  printf "Usage: \n"
+  printf "\t --progam: Specify which program to install\n"
+  printf "\t --version: Specify which version to use (must be available in conda repo)\n"
+}
+
+# refer to https://stackoverflow.com/questions/22025793/using-getopts-to-pick-up-whole-word-flags
+while [ $# -gt 0 ]; do
+  case $1 in
+    --program) program="$2"; shift ;;
+    --version) version="$2"; shift ;;
+    *) print_usage; exit 1 ;;
+  esac
+  shift
+done
+
+echo "Installing $program==$version";
+
+source ~/miniconda3/bin/activate;
+echo "Conda activated";
+
+conda activate base;
+
+
+function install_and_link(){
+  conda install -c conda-forge ${program}=${version} -y;
+  ln -sf ~/miniconda3/bin/${program} ~/.local/bin/${program};
+}
+
+if conda list | grep -q "${program}.*${version}"; then
+  echo "${program} ${version} is already installed on conda. Won't do anything";
+else
+  echo "${program} ${version} is not installed. Will install to base";
+  install_and_link;
+fi
--- setup_ubuntu_server.sh
+++ setup_ubuntu_server.sh
@@ -1,14 +1,14 @@
-bash scripts/conda.sh
-bash scripts/tmux.sh
-bash scripts/pynvim.sh
-bash scripts/zsh.sh
-bash scripts/git.sh
+bash scripts/install_conda.sh;
+bash scripts/setup_pynvim.sh;
+bash scripts/install_using_conda.sh --program git --version 2.40.1;
+bash scripts/install_using_conda.sh --program tmux --version 3.3a;
+bash scripts/install_using_conda.sh --program zsh --version 5.9;
 
 # files
-ln -sf ~/dotfiles/ubuntu-server/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/ubuntu-server/.bashrc ~/.bashrc;
+ln -sf ~/dotfiles/.vimrc ~/.vimrc;
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf;
 
 # folders
-ln -snf ~/dotfiles/.ipython ~/.ipython
-ln -snf ~/dotfiles/nvim-lite ~/.config/nvim
+ln -snf ~/dotfiles/.ipython ~/.ipython;
+ln -snf ~/dotfiles/nvim-lite ~/.config/nvim;
