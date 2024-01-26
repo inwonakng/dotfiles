52e3df8d036fd3f09abbe752032e770b1d728b17
inwonakng <inwon.kang04@gmail.com>
2024-01-26 11:54:03 -0500

    removing zsh from conda install

--- setup_ubuntu_server.sh
+++ setup_ubuntu_server.sh
@@ -2,7 +2,7 @@ bash scripts/install_conda.sh;
 bash scripts/setup_pynvim.sh;
 bash scripts/install_using_conda.sh --program git --version 2.40.1;
 bash scripts/install_using_conda.sh --program tmux --version 3.3a;
-bash scripts/install_using_conda.sh --program zsh --version 5.9;
+# bash scripts/install_using_conda.sh --program zsh --version 5.9;
 
 # files
 ln -sf ~/dotfiles/ubuntu-server/.bashrc ~/.bashrc;
