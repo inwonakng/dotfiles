0eb0e6c5dea56885fdd20c5be3e4fe73644623d5
inwonakng <inwon.kang04@gmail.com>
2024-02-14 03:47:42 -0500

    tmux modified

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,4 +1,10 @@
-bash scripts/conda.sh
+bash scripts/install_conda.sh
+
+# folders
+ln -snf ~/dotfiles/.ipython ~/.ipython
+ln -snf ~/dotfiles/nvim ~/.config/nvim
+ln -snf ~/dotfiles/wezterm ~/.config/wezterm
+ln -snf ~/dotfiles/kitty ~/.config/kitty
 
 # files
 ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
@@ -7,8 +13,4 @@ ln -sf ~/dotfiles/osx/.latexmkrc ~/.latexmkrc
 ln -sf ~/dotfiles/osx/.hammerspoon/init.lua ~/.hammerspoon/init.lua
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-
-# folders
-ln -snf ~/dotfiles/.ipython ~/.ipython
-ln -snf ~/dotfiles/nvim ~/.config/nvim
-ln -snf ~/dotfiles/wezterm ~/.config/wezterm
+ln -sf ~/dotfiles/kitty/kitty-mac.conf ~/.config/kitty/kitty.conf
