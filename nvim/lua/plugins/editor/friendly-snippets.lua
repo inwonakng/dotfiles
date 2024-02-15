094871e6d8a7b807808cddbc31a47a028c52cb49
inwonakng <inwon.kang04@gmail.com>
2024-02-15 14:00:49 -0500

    don't need friendly snipps..

--- nvim/lua/plugins/editor/friendly-snippets.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "rafamadriz/friendly-snippets",
-  config = function()
-    local luasnip_loader = require("luasnip.loaders.from_vscode")
-    -- Custom snippets first.
-    luasnip_loader.lazy_load({ paths = { "./snippets" }})
-    luasnip_loader.lazy_load()
-  end,
-}
