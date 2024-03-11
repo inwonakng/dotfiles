cf196ab0e26b6d82e66d135d4e223887ec5c7878
inwonakng <inwon.kang04@gmail.com>
2024-03-10 22:37:44 -0400

    adding keys for git conflict

--- nvim/lua/plugins/editor/git-conflict.lua
+++ nvim/lua/plugins/editor/git-conflict.lua
@@ -2,4 +2,8 @@ return {
   "akinsho/git-conflict.nvim",
   version = "*",
   config = true,
+  keys = {
+    {"<leader>gcr", "<cmd>GitConflictRefresh<cr>", desc="Refresh Git conflicts"},
+    -- {"<leader>gct", "<cmd>GitConflictRefresh<cr>", desc="Refresh Git conflicts"},
+  }
 }
