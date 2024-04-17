259574008f2c59d8ada8190d9e85c02932514aab
inwonakng <inwon.kang04@gmail.com>
2024-04-17 17:24:50 -0400

    fzf lua file finder enhancement

--- nvim-lite/lua/plugins/navigation/fzf-lua.lua
+++ nvim-lite/lua/plugins/navigation/fzf-lua.lua
@@ -1,5 +1,35 @@
 local fzf_winopts = require("utils.fzf_winopts")
 
+local get_fzf_opts = function()
+  local cmd = nil
+  local fzf_opts = nil
+  if vim.fn.executable("fd") == 1 then
+    local fzfutils = require("fzf-lua.utils")
+    -- fzf-lua.defaults#defaults.files.fd_opts
+    cmd = string.format(
+      [[fd --color=never --type f --hidden --follow --exclude .git -x printf "{}: {/} %s\n"]],
+      -- [[fd --color=never --type f --follow --exclude .git -x echo {} | awk -F/ '{printf "%%s: ", $0; printf "%%s ", $NF; gsub(/^\.\//,"",$0); gsub($NF,"",$0); printf "%s ", $0; print ""}']],
+      fzfutils.ansi_codes.grey("{//}")
+    )
+    fzf_opts = {
+      -- process ansi colors
+      ["--ansi"] = "",
+      ["--with-nth"] = "2..",
+      ["--delimiter"] = "\\s",
+      ["--tiebreak"] = "begin,index",
+    }
+    -- opts._fmt = opts._fmt or {}
+    -- opts._fmt.from = function(entry, _opts)
+    --   local s = fzfutils.strsplit(entry, ' ')
+    --   return s[3]
+    -- end
+  end
+  return {
+    cmd = cmd,
+    fzf_opts = fzf_opts,
+  }
+end
+
 return {
   "ibhagwan/fzf-lua",
   -- optional for icon support
@@ -15,32 +45,32 @@ return {
         -- delete or modify is sufficient
         builtin = {
           -- neovim `:tmap` mappings for the fzf win
-          ["<F1>"]        = "toggle-help",
-          ["<F2>"]        = "toggle-fullscreen",
+          ["<F1>"] = "toggle-help",
+          ["<F2>"] = "toggle-fullscreen",
           -- Only valid with the 'builtin' previewer
-          ["<F3>"]        = "toggle-preview-wrap",
-          ["<F4>"]        = "toggle-preview",
+          ["<F3>"] = "toggle-preview-wrap",
+          ["<F4>"] = "toggle-preview",
           -- Rotate preview clockwise/counter-clockwise
-          ["<F5>"]        = "toggle-preview-ccw",
-          ["<F6>"]        = "toggle-preview-cw",
-          ["}"]    = "preview-page-down",
-          ["{"]      = "preview-page-up",
-          ["<S-left>"]    = "preview-page-reset",
+          ["<F5>"] = "toggle-preview-ccw",
+          ["<F6>"] = "toggle-preview-cw",
+          ["}"] = "preview-page-down",
+          ["{"] = "preview-page-up",
+          ["<S-left>"] = "preview-page-reset",
         },
         fzf = {
           -- fzf '--bind=' options
           -- ["ctrl-z"]      = "abort",
           -- ["ctrl-u"]      = "unix-line-discard",
-          ["ctrl-d"]      = "half-page-down",
-          ["ctrl-u"]      = "half-page-up",
-          ["ctrl-a"]      = "beginning-of-line",
-          ["ctrl-e"]      = "end-of-line",
-          ["alt-a"]       = "toggle-all",
+          ["ctrl-d"] = "half-page-down",
+          ["ctrl-u"] = "half-page-up",
+          ["ctrl-a"] = "beginning-of-line",
+          ["ctrl-e"] = "end-of-line",
+          ["alt-a"] = "toggle-all",
           -- Only valid with fzf previewers (bat/cat/git/etc)
-          ["f3"]          = "toggle-preview-wrap",
-          ["f4"]          = "toggle-preview",
-          ["}"]  = "preview-page-down",
-          ["{"]    = "preview-page-up",
+          ["f3"] = "toggle-preview-wrap",
+          ["f4"] = "toggle-preview",
+          ["}"] = "preview-page-down",
+          ["{"] = "preview-page-up",
         },
       },
     })
@@ -56,16 +86,20 @@ return {
     {
       "<leader><space>",
       function()
-        require("fzf-lua").files()
-      end,
-      desc = "Search Files",
-    },
-    {
-      "<leader>/",
-      function()
-        require("fzf-lua").live_grep()
+        local opts = {}
+        opts.winopts = fzf_winopts.large.vertical
+        require("fzf-lua").live_grep(opts)
       end,
       desc = "Search File Contents",
+    },
+    {
+      "<leader>ff",
+      function()
+        local opts = get_fzf_opts()
+        opts.winopts = fzf_winopts.large.vertical
+        require("fzf-lua").files(opts)
+      end,
+      desc = "Search Files",
     },
     {
       "<leader>sb",
