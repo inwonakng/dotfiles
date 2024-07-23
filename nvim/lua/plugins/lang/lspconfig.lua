83716f23d1168a5457dfad77e254badd641d01df
inwonakng <inwon.kang04@gmail.com>
2024-07-22 20:40:20 -0400

    update lsp

--- nvim/lua/plugins/lang/lspconfig.lua
+++ nvim/lua/plugins/lang/lspconfig.lua
@@ -1,122 +1,20 @@
 return {
   "neovim/nvim-lspconfig",
-  ---@class PluginLspOpts
-  opts = {
-    ---@type lspconfig.options
-    servers = {
-      -- pyright will be automatically installed with mason and loaded with lspconfig
-      pyright = {
-        settings = {
-          python = {
-            analysis = {
-              useLibraryCodeForTypes = true,
-              typeCheckingMode = "off",
-              diagnosticMode = "off",
-              diagnosticSeverityOverrides = {
-                reportUnusedVariable = "warning", -- or anything
-              },
-            },
-          },
-        },
-      },
-      -- tsserver will be automatically installed with mason and loaded with lspconfig
-      ---@type lspconfig.options.tsserver
-      tsserver = {
-        keys = {
-          {
-            "<leader>co",
-            function()
-              vim.lsp.buf.code_action({
-                apply = true,
-                context = {
-                  only = { "source.organizeImports.ts" },
-                  diagnostics = {},
-                },
-              })
-            end,
-            desc = "Organize Imports",
-          },
-          {
-            "<leader>cR",
-            function()
-              vim.lsp.buf.code_action({
-                apply = true,
-                context = {
-                  only = { "source.removeUnused.ts" },
-                  diagnostics = {},
-                },
-              })
-            end,
-            desc = "Remove Unused Imports",
-          },
-        },
-        settings = {
-          typescript = {
-            format = {
-              indentSize = vim.o.shiftwidth,
-              convertTabsToSpaces = vim.o.expandtab,
-              tabSize = vim.o.tabstop,
-            },
-          },
-          javascript = {
-            format = {
-              indentSize = vim.o.shiftwidth,
-              convertTabsToSpaces = vim.o.expandtab,
-              tabSize = vim.o.tabstop,
-            },
-          },
-          completions = {
-            completeFunctionCalls = true,
-          },
-        },
-      },
-      jsonls = {
-        -- lazy-load schemastore when needed
-        on_new_config = function(new_config)
-          new_config.settings.json.schemas = new_config.settings.json.schemas or {}
-          vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
-        end,
-        settings = {
-          json = {
-            format = {
-              enable = true,
-            },
-            validate = { enable = true },
-          },
-        },
-      },
-      rust_analyzer = {},
-      taplo = {
-        keys = {
-          {
-            "K",
-            function()
-              if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
-                require("crates").show_popup()
-              else
-                vim.lsp.buf.hover()
-              end
-            end,
-            desc = "Show Crate Documentation",
-          },
-        },
-      },
-    },
-    setup = {
-      rust_analyzer = function()
-        return true
-      end,
-    },
-    diagnostics = {
+  opts = function(_, opts)
+    local servers = { "pyright", "basedpyright", "ruff", "ruff_lsp", ruff, lsp }
+    for _, server in ipairs(servers) do
+      opts.servers[server] = opts.servers[server] or {}
+      opts.servers[server].enabled = server == lsp or server == ruff
+    end
+    opts["diagnostics"] = {
       underline = true,
       virtual_text = false,
       virtual_lines = {
         only_current_line = true,
         highlight_whole_line = false,
       },
-    },
+    }
     -- autoformat = false,
-    severity_sort = true,
-  },
+    opts["severity_sort"] = true
+  end,
 }
-
