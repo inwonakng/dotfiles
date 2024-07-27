3dcbeb8b130c4dc45f8a1985f1268cb4d8b2655c
inwonakng <inwon.kang04@gmail.com>
2024-07-27 16:20:36 -0400

    update

--- tmux/edit-in-vim.sh
+++ tmux/edit-in-vim.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 # copied from https://dev.to/acro5piano/edit-tmux-output-with-editor-i1c
 
 file=`mktemp`.sh
-
 tmux capture-pane -pS -32768 > $file
 tmux new-window -n:mywindow "$EDITOR '+ normal G $' $file"
