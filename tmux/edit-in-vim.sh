8be2424a192a53a33ee82e3cb805d66ea0ee8821
inwonakng <inwon.kang04@gmail.com>
2024-07-27 16:18:01 -0400

    checking if pull wil work

--- tmux/edit-in-vim.sh
+++ tmux/edit-in-vim.sh
@@ -3,5 +3,6 @@
 # copied from https://dev.to/acro5piano/edit-tmux-output-with-editor-i1c
 
 file=`mktemp`.sh
+
 tmux capture-pane -pS -32768 > $file
 tmux new-window -n:mywindow "$EDITOR '+ normal G $' $file"
