181e796b32873248b88ba5414b7a70823d40bf1e
inwonakng <inwon.kang04@gmail.com>
2024-08-18 20:38:14 -0400

    overseer now knows about shell scripts

--- nvim/lua/plugins/terminal/overseer.lua
+++ nvim/lua/plugins/terminal/overseer.lua
@@ -40,16 +40,17 @@ return {
         winblend = 0,
       },
     },
+    templates = { "builtin", "user.shell_scripts" },
   },
   -- stylua: ignore
   keys = {
     { "<leader>ow", "<cmd>OverseerToggle<cr>",      desc = "Task list" },
-    { "<leader>oo", "<cmd>OverseerRun<cr>",         desc = "Run task" },
+    { "<leader>or", "<cmd>OverseerRun<cr>",         desc = "Run task" },
     { "<leader>oq", "<cmd>OverseerQuickAction<cr>", desc = "Action recent task" },
     { "<leader>oi", "<cmd>OverseerInfo<cr>",        desc = "Overseer Info" },
     { "<leader>ob", "<cmd>OverseerBuild<cr>",       desc = "Task builder" },
-    { "<leader>ot", "<cmd>OverseerTaskAction<cr>",  desc = "Task action" },
+    -- { "<leader>oa", "<cmd>OverseerTaskAction<cr>",  desc = "Task action" },
     { "<leader>oc", "<cmd>OverseerClearCache<cr>",  desc = "Clear cache" },
-    { "<leader>ol", "<cmd>OverseerLoadBundle<cr>", desc = "Action recent task" },
+    { "<leader>ol", "<cmd>OverseerLoadBundle<cr>",  desc = "Action recent task" },
   },
 }
