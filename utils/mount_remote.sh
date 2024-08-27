24c553dadd13f30c592e2cd75e54cb60bba6256d
inwonakng <inwon.kang04@gmail.com>
2024-08-27 18:58:48 -0400

    update mount script

--- utils/mount_remote.sh
+++ utils/mount_remote.sh
@@ -1,10 +1,10 @@
 # /usr/bin/bash
 
-mkdir -p ~/mounts/idea/tabrfm
-sshfs idea-node-05:/home/kangi/tab-reprog-fm ~/mounts/idea/tabrfm -o follow_symlinks,kill_on_unmount,reconnect
+mkdir -p ~/mount/idea/tabrfm
+sshfs idea-node-05:/home/kangi/tab-reprog-fm ~/mount/idea/tabrfm -o follow_symlinks,kill_on_unmount
 
-mkdir -p ~/mounts/silkworm/tabrfm
-sshfs silkworm:/home/kangi/tab-reprog-fm ~/mounts/silkworm/tabrfm -o follow_symlinks,kill_on_unmount,reconnect
+mkdir -p ~/mount/silkworm/tabrfm
+sshfs silkworm:/home/kangi/tab-reprog-fm ~/mount/silkworm/tabrfm -o follow_symlinks,kill_on_unmount
 
-mkdir -p ~/mounts/cci/tabrfm
-sshfs cci:/gpfs/u/home/DDTD/DDTDkngn/scratch/tab-reprog-fm ~/mounts/cci/tabrfm -o follow_symlinks,kill_on_unmount,reconnect
+mkdir -p ~/mount/cci/tabrfm
+sshfs cci:/gpfs/u/home/DDTD/DDTDkngn/scratch/tab-reprog-fm ~/mount/cci/tabrfm -o follow_symlinks,kill_on_unmount
