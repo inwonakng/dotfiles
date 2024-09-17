685c94541dfbe9d07e114963b95e8fd2599ffa8e
inwonakng <inwon.kang04@gmail.com>
2024-09-17 15:50:45 -0400

    fixed lsp issue!

--- nvim/lua/plugins/lang/lspconfig.lua
+++ nvim/lua/plugins/lang/lspconfig.lua
@@ -1,3 +1,6 @@
+local lsp = vim.g.lazyvim_python_lsp or "basedpyright"
+local ruff = vim.g.lazyvim_python_ruff or "ruff"
+
 return {
   "neovim/nvim-lspconfig",
   opts = function(_, opts)
@@ -10,80 +13,80 @@ return {
       { "gy", "<cmd>FzfLua lsp_typedefs        jump_to_single_result=true ignore_current_line=true<cr>", desc = "Goto T[y]pe Definition" },
     })
 
-    return {
-      codelens = {
-        enabled = false,
+    opts.codelens = {
+      enabled = false,
+    }
+    opts.diagnostics = {
+      underline = true,
+      signs = true,
+      virtual_text = false,
+      float = {
+        show_header = true,
+        source = "always",
+        border = "rounded",
+        focusable = true,
       },
-      diagnostics = {
-        underline = true,
-        signs = true,
-        virtual_text = false,
-        float = {
-          show_header = true,
-          source = "always",
-          border = "rounded",
-          focusable = true,
-        },
-        update_in_insert = false, -- default to false
-        severity_sort = false, -- default to false
-      },
-      inlay_hints = {
-        enabled = false,
-      },
-      servers = {
-        ruff = {
-          cmd_env = { RUFF_TRACE = "messages" },
-          init_options = {
-            settings = {
-              logLevel = "error",
-            },
-          },
-          keys = {
-            {
-              "<leader>co",
-              LazyVim.lsp.action["source.organizeImports"],
-              desc = "Organize Imports",
-            },
-          },
-        },
-        basedpyright = {
-          settings = {
-            pyright = {
-              -- Using Ruff's import organizer
-              disableOrganizeImports = true,
-            },
-            basedpyright = {
-              analysis = {
-                -- possible values: off, basic, standard
-                typeCheckingMode = "off",
-              },
-            },
-          },
-        },
-        taplo = {
-          keys = {
-            {
-              "K",
-              function()
-                if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
-                  require("crates").show_popup()
-                else
-                  vim.lsp.buf.hover()
-                end
-              end,
-              desc = "Show Crate Documentation",
-            },
-          },
+      update_in_insert = false, -- default to false
+      severity_sort = false, -- default to false
+    }
+    opts.inlay_hints = {
+      enabled = false,
+    }
+
+    opts.servers.ruff = {
+      cmd_env = { RUFF_TRACE = "messages" },
+      init_options = {
+        settings = {
+          logLevel = "error",
         },
       },
-      setup = {
-        ruff = function()
-          LazyVim.lsp.on_attach(function(client, _)
-            -- Disable hover in favor of Pyright
-            client.server_capabilities.hoverProvider = false
-          end, ruff)
-        end,
+      keys = {
+        {
+          "<leader>co",
+          LazyVim.lsp.action["source.organizeImports"],
+          desc = "Organize Imports",
+        },
       },
     }
+    opts.servers.basedpyright = {
+      settings = {
+        pyright = {
+          -- Using Ruff's import organizer
+          disableOrganizeImports = true,
+        },
+        basedpyright = {
+          analysis = {
+            -- possible values: off, basic, standard
+            typeCheckingMode = "off",
+          },
+        },
+      },
+    }
+    -- opts.servers.taplo = {
+    --   keys = {
+    --     {
+    --       "K",
+    --       function()
+    --         if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
+    --           require("crates").show_popup()
+    --         else
+    --           vim.lsp.buf.hover()
+    --         end
+    --       end,
+    --       desc = "Show Crate Documentation",
+    --     },
+    --   },
+    -- }
+
+    opts.setup = {
+      [ruff] = function()
+        LazyVim.lsp.on_attach(function(client, _)
+          -- Disable hover in favor of Pyright
+          client.server_capabilities.hoverProvider = false
+        end, ruff)
+      end,
+    }
+
+    return opts
   end,
 }
