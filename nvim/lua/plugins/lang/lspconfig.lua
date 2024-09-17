a190ff765cab4486fbb874a6b78a23070a63abd0
inwonakng <inwon.kang04@gmail.com>
2024-09-17 15:50:58 -0400

    update

--- nvim/lua/plugins/lang/lspconfig.lua
+++ nvim/lua/plugins/lang/lspconfig.lua
@@ -62,21 +62,21 @@ return {
         },
       },
     }
-    -- opts.servers.taplo = {
-    --   keys = {
-    --     {
-    --       "K",
-    --       function()
-    --         if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
-    --           require("crates").show_popup()
-    --         else
-    --           vim.lsp.buf.hover()
-    --         end
-    --       end,
-    --       desc = "Show Crate Documentation",
-    --     },
-    --   },
-    -- }
+    opts.servers.taplo = {
+      keys = {
+        {
+          "K",
+          function()
+            if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
+              require("crates").show_popup()
+            else
+              vim.lsp.buf.hover()
+            end
+          end,
+          desc = "Show Crate Documentation",
+        },
+      },
+    }
 
     opts.setup = {
       [ruff] = function()
