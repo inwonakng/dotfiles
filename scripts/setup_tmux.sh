75288e11347a58c78b906a9cc31b58f212493c69
inwonakng <inwon.kang04@gmail.com>
2024-10-14 18:34:44 -0400

    fixed tmux..

--- scripts/setup_tmux.sh
+++ scripts/setup_tmux.sh
@@ -2,7 +2,3 @@
 mkdir -p $HOME/.config
 ln -snf $HOME/dotfiles/tmux $HOME/.config/tmux
 
-# manually control catpuccin -- some issues with TPM
-mkdir -p $HOME/.config/tmux/plugins/catppuccin
-git clone https://github.com/catppuccin/tmux.git $HOME/.config/tmux/plugins/catppuccin/tmux
-
