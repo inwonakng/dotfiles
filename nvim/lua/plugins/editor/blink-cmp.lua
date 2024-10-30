5c8769e92f8b9f65d6a9e76e18e69e310a22f30b
inwonakng <inwon.kang04@gmail.com>
2024-10-30 13:45:03 -0400

    update blink

--- nvim/lua/plugins/editor/blink-cmp.lua
+++ nvim/lua/plugins/editor/blink-cmp.lua
@@ -1,21 +1,10 @@
 return {
   "saghen/blink.cmp",
   lazy = false, -- lazy loading handled internally
-  -- optional: provides snippets for the snippet source
   dependencies = { "rafamadriz/friendly-snippets", "saghen/blink.compat" },
-  -- use a release tag to download pre-built binaries
   version = "v0.*",
-  -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  -- build = 'cargo build --release',
-  -- If you use nix, you can build from source using latest nightly rust with:
-  -- build = 'nix run .#build-plugin',
-
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
   opts = {
     keymap = {
-      -- show = "<D-c>",
-      -- accept = "<C-CR>",
       ["<S-CR>"] = { "hide" },
       ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
       ["<C-CR>"] = { "select_and_accept" },
@@ -23,28 +12,8 @@ return {
       ["<C-p>"] = { "select_prev", "fallback" },
       ["<C-b>"] = { "scroll_documentation_up", "fallback" },
       ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-      -- select_next = { "<Tab>", "<Down>", "<C-n>" },
-      -- select_prev = { "<S-Tab>", "<Up>", "<C-p>" },
-      -- select_next = { "<C-n>" },
-      -- select_prev = { "<C-p>" },
-      -- scroll_documentation_down = "<PageDown>",
-      --  = "<PageUp>",
     },
-    highlight = {
-      -- sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- useful for when your theme doesn't support blink.cmp
-      -- will be removed in a future release, assuming themes add support
-      -- use_nvim_cmp_as_default = true,
-    },
-    -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-    -- adjusts spacing to ensure icons are aligned
     nerd_font_variant = "mono",
-
-    -- experimental auto-brackets support
-    -- accept = { auto_brackets = { enabled = true } }
-
-    -- experimental signature help support
-    -- trigger = { signature_help = { enabled = true } }
     sources = {
       completion = { "lsp", "path", "snippets", "buffer" },
       providers = {
@@ -101,13 +70,7 @@ return {
             icon = "󰩫"
           elseif source == "Buffer" or (client == "basics_ls" and ctx.kind == "Text") then
             icon = "󰦨"
-            -- elseif client == "emmet_language_server" then
-            --   icon = "󰯸"
           end
-
-          -- FIX highlight for Tokyonight
-          -- local iconHl = vim.g.colors_name:find("tokyonight") and "BlinkCmpKind" or "BlinkCmpKind" .. ctx.kind
-
           return {
             { icon .. ctx.icon_gap, hl_group = "BlinkCmpKind" .. ctx.kind },
             {
