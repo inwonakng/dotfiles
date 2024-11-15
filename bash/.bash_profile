e80ec4a5bb710d205672bf9c64503c74cdcd2ae9
inwonakng <inwon.kang04@gmail.com>
2024-11-15 10:04:08 -0500

    installer for slurm completions

--- bash/.bash_profile
+++ bash/.bash_profile
@@ -17,6 +17,10 @@ if [ -f "$HOME/.config/ripgrep/config" ]; then
   export RIPGREP_CONFIG_PATH="$HOME/.config/ripgrep/config"
 fi
 
-if [ -f ~/.git-completion.bash ]; then
-  . ~/.git-completion.bash
+if [ -f "$HOME/.bash_utils/completions/git.bash" ]; then
+  . "$HOME/.bash_utils/completions/git.bash"
+fi
+
+if [ -f "$HOME/.bash_utils/completions/slurm.bash" ]; then
+  . "$HOME/.bash_utils/completions/slurm.bash"
 fi
--- bash/scripts/install_completion.sh
+++ bash/scripts/install_completion.sh
@@ -1,4 +1,10 @@
+mkdir -p "$HOME/.bash_utils/completions"
+
 # install git completions
 VERSION=$(git --version | awk '{print $3}' | sed 's/^/v/')
 URL="https://raw.githubusercontent.com/git/git/$VERSION/contrib/completion/git-completion.bash"
-curl $URL -o "$HOME/.git-completion.bash"
+curl $URL -o "$HOME/.bash_utils/completions/git.bash"
+
+# install slurm completions
+URL="https://raw.githubusercontent.com/damienfrancois/slurm-helper/refs/heads/master/slurm_completion.sh"
+curl $URL -o "$HOME/.bash_utils/completions/slurm.bash"
