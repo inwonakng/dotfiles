52b6f92f2ce386a46ee67b3ecbddd2059b3817f2
inwonakng <inwon.kang04@gmail.com>
2025-03-18 00:43:40 -0400

    got snippets to work

--- litenvim/lua/plugins/blink.lua
+++ litenvim/lua/plugins/blink.lua
@@ -1,43 +1,49 @@
 return {
-  "saghen/blink.cmp",
-  dependencies = "rafamadriz/friendly-snippets",
-  version = "*",
-  build = 'cargo build --release',
+	"saghen/blink.cmp",
+	dependencies = "rafamadriz/friendly-snippets",
+	version = "*",
+	build = "cargo build --release",
 
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    cmdline = {
-      enabled = false,
-    },
-    keymap = {
-      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-      ["<C-e>"] = { "hide", "fallback" },
-      ["<CR>"] = { "accept", "fallback" },
-      ["<Tab>"] = { "snippet_forward", "fallback" },
-      ["<S-Tab>"] = { "snippet_backward", "fallback" },
-      ["<Up>"] = { "select_prev", "fallback" },
-      ["<Down>"] = { "select_next", "fallback" },
-      ["<C-p>"] = { "select_prev", "fallback" },
-      ["<C-n>"] = { "select_next", "fallback" },
-      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-      ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
-    },
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = "mono",
-    },
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer", "markdown" },
-      providers = {
-        markdown = {
-          name = "RenderMarkdown",
-          module = "render-markdown.integ.blink",
-          fallbacks = { "lsp" },
-        }
-      }
-    },
-  },
-  opts_extend = { "sources.default" },
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+		cmdline = {
+			enabled = false,
+		},
+		keymap = {
+			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide", "fallback" },
+			["<CR>"] = { "accept", "fallback" },
+			["<Tab>"] = { "snippet_forward", "fallback" },
+			["<S-Tab>"] = { "snippet_backward", "fallback" },
+			["<Up>"] = { "select_prev", "fallback" },
+			["<Down>"] = { "select_next", "fallback" },
+			["<C-p>"] = { "select_prev", "fallback" },
+			["<C-n>"] = { "select_next", "fallback" },
+			["<C-b>"] = { "scroll_documentation_up", "fallback" },
+			["<C-f>"] = { "scroll_documentation_down", "fallback" },
+			["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+		},
+		appearance = {
+			use_nvim_cmp_as_default = true,
+			nerd_font_variant = "mono",
+		},
+		completion = {
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 500,
+			},
+		},
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer", "markdown" },
+			providers = {
+				markdown = {
+					name = "RenderMarkdown",
+					module = "render-markdown.integ.blink",
+					fallbacks = { "lsp" },
+				},
+			},
+		},
+	},
+	opts_extend = { "sources.default" },
 }
