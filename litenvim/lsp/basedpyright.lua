b3cf782ba3fb881fb42587750c6397ab041be3d0
inwonakng <inwon.kang04@gmail.com>
2025-04-15 23:44:35 -0400

    update root_dir condition

--- litenvim/lsp/basedpyright.lua
+++ litenvim/lsp/basedpyright.lua
@@ -1,12 +1,23 @@
 return {
 	cmd = { "basedpyright-langserver", "--stdio" },
 	filetypes = { "python" },
+	root_markers = {
+		"pyproject.toml",
+		"setup.py",
+		"setup.cfg",
+		"requirements.txt",
+		"Pipfile",
+		"pyrightconfig.json",
+		".git",
+	},
 	settings = {
 		basedpyright = {
 			analysis = {
 				autoSearchPaths = true,
 				diagnosticMode = "openFilesOnly",
 				useLibraryCodeForTypes = true,
+				-- extraPaths = { vim.fn.getcwd() },
+				-- extraPaths = { "tabkit" },
 				-- diagnosticMode = "off",
 			},
 		},
