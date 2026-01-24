b24f2cf4abcfae3427369e2feecac99ef678a002
inwonakng <inwon.kang04@gmail.com>
2026-01-24 13:43:09 -0500

    simplify tmux config

--- scripts/setup/local.sh
+++ scripts/setup/local.sh
@@ -1,4 +1,4 @@
 ln -snf ~/dotfiles/bash/bashrc/local.sh ~/.bashrc
-ln -snf ~/dotfiles/bash/bashrc/extras.sh ~/.bashrc_extras
 ln -snf ~/dotfiles/bash/.bash_profile ~/.bash_profile
 ln -snf ~/dotfiles/bash/utils ~/.bash_utils
+ln -snf ~/dotfiles/tmux/tmux.conf.local ~/.config/tmux/tmux.conf
--- scripts/setup/remote.sh
+++ scripts/setup/remote.sh
@@ -1,6 +1,5 @@
 # bash stuff
 ln -snf ~/dotfiles/bash/bashrc/remote.sh ~/.bashrc
-ln -snf ~/dotfiles/bash/bashrc/extras.sh ~/.bashrc_extras
 ln -snf ~/dotfiles/bash/.bash_profile ~/.bash_profile
 ln -snf ~/dotfiles/bash/utils ~/.bash_utils
 
