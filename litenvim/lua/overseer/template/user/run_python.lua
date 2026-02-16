e4dce0ac5586e86281073c733994ee247df2ea60
inwonakng <inwon.kang04@gmail.com>
2026-02-16 14:54:11 -0500

    update nvim config

--- litenvim/lua/overseer/template/user/run_python.lua
+++ litenvim/lua/overseer/template/user/run_python.lua
@@ -1,10 +1,19 @@
 return {
 	name = "Run python script",
 	builder = function(params)
-		local file = vim.fn.expand("%:p") -- Get current file's full path
+		local file = vim.fn.expand("%:.") -- Get current file's path relative to cwd
+		local cwd = vim.fn.getcwd()
+		-- Convert path to module: exp/analysis/myscript.py -> exp.analysis.myscript
+		local module = file:gsub("%.py$", ""):gsub("/", ".")
+		local cmd = { "pixi", "run", "python", "-m", module }
+		vim.notify(
+			string.format("Running: %s\nCWD: %s", table.concat(cmd, " "), cwd),
+			vim.log.levels.INFO
+		)
 		return {
-			cmd = { "pixi", "run", "python", file },
-			name = "python " .. vim.fn.expand("%:t"), -- Shows filename in task list
+			cmd = cmd,
+			name = "python " .. vim.fn.expand("%:t"),
+			cwd = cwd,
 		}
 	end,
 	desc = "Run this file as a python script using pixi.",
--- litenvim/lua/plugins/vimtex.lua
+++ litenvim/lua/plugins/vimtex.lua
@@ -9,6 +9,7 @@ return {
     }
     -- only open quickfix when there are *errors*
     vim.g.vimtex_quickfix_open_on_warning = 0
+    vim.g.vimtex_view_skim_sync = 1
   end,
   keys = {
     { "<localLeader>l", "", desc = "+vimtex" },
