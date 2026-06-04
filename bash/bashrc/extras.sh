af509e93dfca1e1750a3de7d93837649654e11a0
inwon <inwon@dhcp-9-31-101-254.watson.ibm.com>
2026-06-04 10:34:14 -0400

    adding shared opencode config

--- bash/bashrc/extras.sh
+++ bash/bashrc/extras.sh
@@ -279,6 +279,7 @@ alias tmux-start="bash ~/dotfiles/tmux/scripts/start.sh"
 alias tmux-kill="bash ~/dotfiles/tmux/scripts/clean-exit.sh"
 # remove global conda from path... i don't use this
 export PATH=$(echo "$PATH" | sed -e 's/:\/software\/anaconda3.24\/bin//g')
+export OPENCODE_CONFIG="$HOME/dotfiles/opencode/machine-specific-config.jsonc"
 
 # if pixi is installed, just add to path. this doesn't auto load the full thing though.
 if [[ -d "$HOME/.pixi" ]]; then
