7407b563bd0d615f5cc9840167aa7fb92c6855b2
inwonakng <inwon.kang04@gmail.com>
2026-08-24 10:51:08 -0400

    change scrolloff setting for regular nvim

--- litenvim/lua/config/options.lua
+++ litenvim/lua/config/options.lua
@@ -34,7 +34,7 @@ vim.opt.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 vim.opt.inccommand = "split"
 vim.opt.cursorline = true
-vim.opt.scrolloff = 10
+-- vim.opt.scrolloff = 1
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
